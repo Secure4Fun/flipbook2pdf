4b2ff39348c2c7d22df07b315bec96175e68bae8
Secure4Fun <19522196+Secure4Fun@users.noreply.github.com>
2022-05-19 05:18:45 -0500

    Reworked VS solution, rebuilt virtual environment.

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Mike\source\repos\Secure4Fun\flipbook_scraper\env
+set VIRTUAL_ENV=C:\Users\Mike\source\repos\Secure4Fun\flipbook2pdf\env
 
 if not defined PROMPT set PROMPT=$P$G
 
